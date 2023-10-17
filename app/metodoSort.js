4a175fab90e0e04a0f3eb5388b4478a9d21d7eb7
Matheus Garcia <115594762+DevM-Garcia@users.noreply.github.com>
2023-10-17 20:40:59 -0300

    commit da reflexão
    Gostaria de expor nesse commit uma importante reflexão que tive hoje
    
    RESOLVA SEUS B.O
    
    o código não rodava e eu queria apagar e começar tudo de novo, porém, resolvi procurar o problema novamente, foi ai que descobri que a função tinha sido declarada em dois lugares diferente '-' conclusão ...
    
    Resolvi um b.o, aprendi um pouco mais, e melhorei minha autoestima *-----------------*

---
+++ app/metodoSort.js
@@ -0,0 +1,10 @@
+btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
+
+btnOrdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco)
+
+function ordenarLivrosPorPreco() {
+    livrosOrdenados = livros.sort((a, b) => { a.preco - b.preco })
+    exibirOsLivrosNaTela(livrosOrdenados)
+}
+
+
